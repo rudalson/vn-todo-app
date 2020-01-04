8d6a8860f077cf0a6e8172d3f191d1169d49c497
rudalson <rudalson@gmail.com>
2020-01-04 15:39:55 +0900

    added the first components - statusbar

--- App.vue
+++ App.vue
@@ -1,17 +1,22 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
+    <Statusbar color="#00FF00"/>
+    <text>Hello, World</text>
   </view>
 </template>
+
+<script>
+  import Statusbar from './components/Statusbar';
+  export default {
+    components: {
+      Statusbar
+    }
+  }
+</script>
 
 <style>
 .container {
   background-color: white;
-  align-items: center;
-  justify-content: center;
   flex: 1;
-}
-.text-color-primary {
-  color: blue;
 }
 </style>
